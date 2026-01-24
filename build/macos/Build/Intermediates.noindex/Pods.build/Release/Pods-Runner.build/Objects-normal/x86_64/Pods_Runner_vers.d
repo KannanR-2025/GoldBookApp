7520f258b018144bca8c0ed26aacf732e8b86e6e
“Kannan <kannan.r@datacaliper.com>
2026-01-24 22:44:39 +0530

    Remove build folder from git tracking and update .gitignore for Flutter project

--- build/macos/Build/Intermediates.noindex/Pods.build/Release/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/kannanr/2.Sample_Projects/goldbook_desktop/build/macos/Build/Intermediates.noindex/Pods.build/Release/Pods-Runner.build/module.modulemap \
-  /Users/kannanr/2.Sample_Projects/goldbook_desktop/build/macos/Build/Intermediates.noindex/Pods.build/Release/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
